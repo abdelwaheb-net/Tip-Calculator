2e24e5fdcd162d2a9859e807cc908829edefc5b1
Abdelwaheb Ammar <abdelwaheb_ammar@hotmail.fr>
2025-05-05 00:48:41 +0100

    calculate amount of the bill

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ $(document).ready(function () {
   
   $("#calculateBtn").on("click", function () {
     let bill = $("#bill").val(); 
-    
-  
+    let tip=$("#tip").val();
+    let people=$("#people").val();
+    let buttoncalculate=$("#calculateBtn").val();
+    let buttonreinitialise=$("#resetBtn").val();
+    let amount=$("#tipAmount").val();
+    let totalamount=$("#totalAmount").val();
+    let parpersonne=$("#perPerson").val();
+
+    amount=bill*(tip/100);
+    $("#tipAmount").text(amount.toFixed(2)+ " €");
+
   });
 });

035a6f94836976bdf861584c4826d6693abfd8fb
abdelwaheb-net <abdelwaheb_ammar@hotmail.fr>
2025-05-06 00:20:32 +0100

    calculate custom amount

--- script.js
+++ script.js
@@ -3,15 +3,31 @@ $(document).ready(function () {
   $("#calculateBtn").on("click", function () {
     let bill = $("#bill").val(); 
     let tip=$("#tip").val();
+    let customTip = $("#customTip").val();
     let people=$("#people").val();
     let buttoncalculate=$("#calculateBtn").val();
     let buttonreinitialise=$("#resetBtn").val();
     let amount=$("#tipAmount").val();
     let totalamount=$("#totalAmount").val();
     let parpersonne=$("#perPerson").val();
-
-    amount=bill*(tip/100);
+if ($("#tip").val() == "custom"){
+amount = bill * (customTip / 100);
+}
+else
+{
+  amount = bill * (tip / 100);
+}
+    
     $("#tipAmount").text(amount.toFixed(2)+ " €");
 
+  });
+
+  $("#tip").on("change", function () {
+    if($("#tip").val()=='custom'){
+      $("#customTip").show();
+    }
+    else{
+      $("#customTip").hide();
+    }
   });
 });
